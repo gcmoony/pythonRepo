dc6473664d46735080a21ed73e86312ac3c0d59f
unknown <gec68@nau.edu>
2019-05-26 20:28:52 -0700

    Creation of Node class and Stack testing

---
+++ Data Structure/Node.py
@@ -0,0 +1,42 @@
+class Node():
+    """
+    This is a Node that stores an object and points to another node
+    """
+
+    def __init__(self, anObject):
+        """
+        Creates a node object using variable 'anObject' as information. Next
+        node is set to None
+        """
+        self.nodeInfo = anObject
+        self.nextNode = None
+
+    def  copyNode(self):
+        """
+        Creates a copy of the current node and returns a node object
+        """
+        return Node(self.nodeInfo)
+
+    def setNextNode(self, aNode):
+        """
+        Points current node to another node object
+        """
+        self.nextNode = aNode
+
+    def getNextNode(self):
+        """
+        Returns next node
+        """
+        return self.nextNode
+
+    def clearNextNode(self):
+        """
+        Sets the next node to None
+        """
+        self.nextNode = None
+
+    def __str__(self):
+        """
+        Returns data stored inside the node
+        """
+        return self.nodeInfo
---
+++ Data Structure/driver_Stack.py
@@ -0,0 +1,32 @@
+import Node
+
+def main():
+
+    def printList(aList):
+        for item in aList:
+            print(item)
+        print("\n")
+
+    # Create node instances
+    personNode1 = Node.Node("Sally")
+    personNode2 = Node.Node("Jerry")
+    personNode3 = Node.Node("Bob")
+    personNode4 = Node.Node("Mary")
+
+    personList = [personNode1, personNode2,
+                  personNode3, personNode4]
+    printList(personList)
+
+    # Test Copy
+    personCopyList = []
+    for person in personList:
+        personCopyList.append(person.copyNode())
+    printList(personCopyList)
+
+    # Test linking
+    personList[0].nextNode = personList[1]
+    print(personList[0].getNextNode())
+
+
+if __name__ == '__main__':
+    main()
