4e947cffc2e612d9c84a08a49827f1f91dd28932
GesusKryst <gec68@nau.edu>
2020-08-22 21:27:35 -0700

    Last push from Ubuntu

---
+++ GUIs/TicTacToeGUI.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+# Create Menu Screen
+def mainMenu():
+    menuWindow = tk.Tk()
+    
+    menuWindow.mainloop()
+
+
+
+def main():
+    mainMenu()
+
+
+if __name__ == "__main__":
+    main()
